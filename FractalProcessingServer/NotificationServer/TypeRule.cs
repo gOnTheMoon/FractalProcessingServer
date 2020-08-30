f1a21e457c26ff8cfbdb86c14a13a528f0d0397d
Elad Zelingher <elad@codesharp.net>
2020-08-29 22:04:01 -0400

    Adding TypeRule

---
+++ FractalProcessingServer/NotificationServer/TypeRule.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace NotificationServer
+{
+    public class TypeRule : IRule
+    {
+        private readonly Type mEventType;
+
+        public TypeRule(Type eventType)
+        {
+            mEventType = eventType;
+        }
+
+        public bool IsActivated(IEvent eventToCheck)
+        {
+            return mEventType.IsInstanceOfType(eventToCheck);
+        }
+    }
+}
